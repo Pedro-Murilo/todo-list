582806d929286d8c80eabeb185584accdca1a9a4
Pedro Murilo <pedromurilocalvi@gmail.com>
2020-12-26 15:54:29 -0300

    Add a funcionality to make todo and edit Main.css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,30 +1,67 @@
 import React, { Component } from 'react';
 
+// Form
+import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
+
+// Tarefas
+
 import './Main.css';
 
 export default class Main extends Component {
   state = {
-    newTask: '',
+    newTodo: '',
+    tarefas: [],
   };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas } = this.state;
+    let { newTodo } = this.state;
+    newTodo = newTodo.trim();
+
+    if (tarefas.indexOf(newTodo) !== -1) return;
+
+    const novasTarefas = [...tarefas];
+    this.setState({
+      tarefas: [...novasTarefas, newTodo],
+    });
+  }
 
   handleChange = (e) => {
     this.setState({
-      newTask: e.target.value,
+      newTodo: e.target.value,
     });
   }
 
   render() {
-    const { newTask } = this.state;
+    const { newTodo, tarefas } = this.state;
 
     return (
       <div className="main">
-        <h1>Task List</h1>
-        <p>{newTask}</p>
+        <h1>Todo List</h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Send</button>
+        <form onSubmit={this.handleSubmit} action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={newTodo}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+              {tarefa}
+              <span className="">
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
